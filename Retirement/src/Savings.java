0d10b88fe446716e2cdae1245797d483f8dc1cec
Stephen Harbeck <42806740+Sharb791@users.noreply.github.com>
2018-10-29 08:07:08 -0400

    Updated Code

--- Retirement/src/Savings.java
+++ Retirement/src/Savings.java
@@ -1,3 +1,4 @@
+
 /////////////////////////////////////////////////////////////////////////////
 //Author:					Stephen Harbeck
 //Creation Date:			9/8/18
@@ -15,33 +16,46 @@ import java.text.DecimalFormat;
 public class Savings {
 
 	public static void main(String[] args) {
-		double numYears;
-		double save;
+		double numYears = 0;
+		double save = 0;
+		int x = 1;
 
 		// Scanner reads in inputs to use them
 		Scanner retire = new Scanner(System.in);
 
 		// Creates a do while loop for the user to input information then loops if the
 		// user puts in an invalid value
+
 		do {
-			System.out.print("Please enter the number of years until you want retire: ");
-			numYears = retire.nextDouble();
 
-			System.out.print("Please enter the amount of money you can save every year: $");
-			save = retire.nextDouble();
+			// Creates statement to check the user inputs
+			try {
 
-			// Sets bounds for what can be entered
-			if ((numYears > 0 && numYears < 100) && save > 0) {
+				System.out.print("Please enter the number of years until you want retire: ");
+				numYears = retire.nextDouble();
+
+				System.out.print("Please enter the amount of money you can save every year: $");
+				save = retire.nextDouble();
+				
+				x = 2;
+				
+
+			} catch (Exception e) {
+				System.out.println("Please enter a valid number!!");
+			} 
+
+			// Checks if the inputs are within the range
+			 
+				 if ((numYears > 0 && numYears < 100) && save > 0) {
 				System.out.println("You will retire in " + numYears + " year(s).");
 				System.out.println("You will save $" + save + " each year.");
 
-				
 				DecimalFormat dot = new DecimalFormat("#.00");
 				double interest;
 
 				// Sets interest rate as a constant
 				final double IN_RATE = .05;
-				
+
 				// Creates the formula for interest
 				interest = (save * IN_RATE) * numYears;
 				System.out.println("There will be $" + dot.format(interest) + " in interest on your savings.");
@@ -49,20 +63,13 @@ public class Savings {
 				double totalSavings;
 
 				// Creates the formula for the total savings
-				totalSavings = (save * numYears) - interest;
+				totalSavings = (save * numYears) + interest;
 				System.out.println("The total amount saved before you retire is $" + dot.format(totalSavings));
 				System.out.println("  ");
-				break;
-
-				// This else statement just prints out an Invalid entry prompt to the user.
-			} else {
-				System.out.println("Please enter a value greater than 0!");
-				System.out.println("  ");
 			}
+			break;
+		} while (x == 1);
 
-			// This will create the loop until valid values are entered
-		} while (true);
-		// Program is terminated
 	}
 
 }
